5dc814a601ae8969a3503b4634f302d3f42fa8bd
jacobjolibois <jacobjolibois@hey.com>
2022-01-18 14:53:11 -0600

    updated the movie components to work with the combineReducers

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -58,7 +58,7 @@ const Movie = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        movies: state.movies,
+        movies: state.movie.movies,
     };
 }
 
--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -23,7 +23,7 @@ const MovieHeader = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        appTitle: state.appTitle,
+        appTitle: state.movie.appTitle,
     };
 }
 
